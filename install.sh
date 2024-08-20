6634ed50495bae1d2293f706817984949e9658ce
RFS <7137874+rfs85@users.noreply.github.com>
2024-08-20 01:46:55 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a directory to store your repositories
+mkdir -p ~/RFS
+cd ~/RFS
+
+sudo apt update -y
+sudo apt install -y netexec
+git clone
+git clone
+git clone
+git clone
+git clone
