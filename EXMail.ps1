d7a96877409b82534e6acb1bc1d38732728ba486
RFS <7137874+rfs85@users.noreply.github.com>
2024-08-20 11:48:03 +0100

    Update EXMail.ps1

--- EXMail.ps1
+++ EXMail.ps1
@@ -7,3 +7,19 @@ Get-Mailbox -resultsize unlimited | Get-MailboxPermission | where {($_.user.tost
 
 
 Get-MessageTrackingLog | more
+
+
+
+$emailPoints = @(
+    @{Point = "EMAIL.1"; Description = "Proteção da confidencialidade do correio eletrónico."},
+    @{Point = "EMAIL.2"; Description = "SPF"},
+    @{Point = "EMAIL.3"; Description = "DKIM"},
+    @{Point = "EMAIL.4"; Description = "DMARC"}
+    @{Point = "EMAIL.5"; Description = "Anti Spam"}
+    @{Point = "EMAIL.6"; Description = "Relay"}
+    @{Point = "EMAIL.7"; Description = "Politica de throttling"}
+    @{Point = "EMAIL.8"; Description = "Prevenir anexos maliciosos"}
+    @{Point = "EMAIL.9"; Description = "Registo de eventos"}
+    @{Point = "EMAIL.10"; Description = "Restringir receção de mails com domínios locais"}
+    @{Point = "EMAIL.11"; Description = "Implementação de DNSSEC"}
+)
